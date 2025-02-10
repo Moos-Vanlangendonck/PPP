2d831cbdd2fe7dd4bfe5a4fce8db200445264a3a
Moos-Vanlangendonck <67058923+Moos-Vanlangendonck@users.noreply.github.com>
2025-02-10 17:06:57 +0100

    Alison Assembly Course

---
+++ bitwise_xor.asm
@@ -0,0 +1,23 @@
+section .data
+    a db 15               
+    b db 10               
+
+section .text
+    global _start
+
+_start:
+    mov rdi, a            
+    imul rsi, 1           
+    xor al, [b]           
+    mov [result], al      
+
+result:
+    mov rdi, result
+    call printf
+
+    jmp exit_program
+
+exit_program:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
